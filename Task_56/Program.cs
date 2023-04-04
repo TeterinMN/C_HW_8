c46fa74667264669934634b25d1c80102926765e
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-04 18:25:17 +0900

    Закончил задачу №56

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Метод для вывода массива в консоль
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Создание двумерного массива
+int[,] array = new int[,]
+{
+    { 1, 4, 7, 2 },
+    { 5, 9, 2, 3 },
+    { 8, 4, 2, 4 },
+    { 5, 2, 6, 7 }
+};
+
+// Вывод исходного массива
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+// Поиск строки с наименьшей суммой элементов
+int minRowIndex = 0;
+int minRowSum = int.MaxValue;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int rowSum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        rowSum += array[i, j];
+    }
+    if (rowSum < minRowSum)
+    {
+        minRowSum = rowSum;
+        minRowIndex = i;
+    }
+}
+
+Console.WriteLine("Строка с наименьшей суммой элементов: " + (minRowIndex + 1));
