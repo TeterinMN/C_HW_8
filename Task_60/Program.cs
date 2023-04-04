2a8072e7db530c47ad5a16a31bab625d1cd8183e
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-04 19:17:57 +0900

    Закончил задачу №60

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Создание трехмерного массива
+int[,,] array = new int[2, 2, 2];
+
+// Заполнение массива значениями
+Random num = new Random();
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        for (int k = 0; k < 2; k++)
+        {
+            array[i, j, k] = num.Next(10, 99);
+        }
+    }
+}
+
+// Вывод массива с добавлением индексов каждого элемента
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        for (int k = 0; k < 2; k++)
+        {
+            Console.Write(array[i, j, k]);
+            Console.Write("(" + i + "," + j + "," + k + ") ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
