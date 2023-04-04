8b3ef0a6ea65308c7a8c73b26044a63145653eb1
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-04 18:10:34 +0900

    Закончил задачу №54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿Console.Clear();
+
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Создание массива
+int[,] array = new int[,]
+{
+    { 1, 4, 7, 2 },
+    { 5, 9, 2, 3 },
+    { 8, 4, 2, 4 }
+};
+
+// Вывод исходного массива
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+// Сортировка элементов строк по убыванию
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, j] < array[i, k])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Упорядоченный массив:");
+PrintArray(array);
